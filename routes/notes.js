2427a6520353ac0d1116e138ecf77f3ef7e5b87e
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-06-04 15:59:25 -0700

    Updated routers in note.js

--- routes/notes.js
+++ routes/notes.js
@@ -8,7 +8,6 @@ const path = require("path");
 // GET Route for retrieving all the notes
 notesRouter.get("/", (req, res) => {
   fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
-    console.log(data);
     if (err) {
       res.json(`Error: ${err}`);
     } else {
@@ -19,7 +18,16 @@ notesRouter.get("/", (req, res) => {
 
 // POST Route for adding notes
 notesRouter.post("/", (req, res) => {
-  fs.writeFile(path.join(__dirname, "./db/db.json"), req.body);
+  fs.writeFile(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify(req.body),
+    "utf8",
+    (err) => {
+      res.json({ success: true });
+      next();
+    }
+  );
+  res.json(req.body);
 });
 
 module.exports = notesRouter;
