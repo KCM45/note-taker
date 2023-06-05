c08837f1db21bf9be0b9b4b0557704c549c870e2
Kama Corey McKenzie <kcm@Kamas-MacBook-Pro.local>
2023-06-05 14:39:53 -0700

    adding id to db.json

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,6 @@ const notesRouter = express.Router();
 const fs = require("fs");
 const path = require("path");
 
-// GET Route for retrieving all the notes
 notesRouter.get("/", (req, res) => {
   fs.readFile(path.join(__dirname, "../db/db.json"), "utf-8", (err, data) => {
     if (err) {
--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get("/notes", (req, res) =>
 // });
 
 // Wildcard route to direct users to a 404 page
-// app.get("*", (req, res) =>
-//   res.sendFile(path.join(__dirname, "public/pages/404.html"))
-// );
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
